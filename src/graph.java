0301f63d3d0b5a7db0460e96ef036cbf179bb6cd
NoobSupp <mascagni.123@gmail.com>
2022-08-22 22:08:23 -0300

    22/08

--- src/graph.java
+++ src/graph.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class graph{
   private int countNodes;
   private int countEdges;
@@ -16,9 +18,20 @@ class graph{
     
     this.adjMatrix[source][sink] = weight;
     this.countEdges++;
-    
   }
 
+  public void addEdgeUnoriented(int u, int v, int w) {
+    if (u < 0 || u > this.countNodes - 1
+        || v < 0 || v > this.countNodes - 1 || w <= 0) {
+      System.err.println("Invalid edge: " + u + v + w);
+      return;
+    }
+    this.adjMatrix[u][v] = w;
+    this.adjMatrix[v][u] = w;
+    this.countEdges += 2;
+  }
+
+  
   public int degree(int node){
     int count = 0;
     if(node<0 || node > this.countNodes-1){
@@ -33,9 +46,88 @@ class graph{
     return count;
   }
 
+  public int highestDegree(){
+    int highest = 0;
 
+    for(int i=0; i<this.adjMatrix.length; i++){
+      int degree = this.degree(i);
+      if(degree(i)>highest){
+        highest=degree;
+      }
+    }
+    return highest;
+  }
 
-  
+  public int lowestDegree(){
+    int lowest = this.adjMatrix.length;
+
+    for(int i=1; i<this.adjMatrix.length; i++){
+      int degree = this.degree(i);
+      if(degree(i)<lowest){
+        lowest=degree;
+      }
+    }
+    return lowest;
+  }
+
+  public graph complement(){
+    graph g2 = new graph(this.countNodes);
+
+    for(int i=0; i<this.adjMatrix.length; i++){
+      for(int j=0; j<this.adjMatrix.length; j++){
+        if(this.adjMatrix[i][j] == 0 && i!=j){
+          g2.addEdge(i, j, 1);
+        }
+      }
+    }
+    return g2;
+  }
+  public float density() {
+    return (float) this.countEdges / (this.countNodes * (this.countNodes - 1));
+  }
+
+  public boolean subGraph(graph g2) {
+    if (g2.countNodes > this.countNodes || g2.countEdges > this.countEdges)
+      return false;
+    for(int i = 0; i < g2.adjMatrix.length; ++i) {
+      for(int j = 0; j < g2.adjMatrix[i].length; ++j) {
+        if(g2.adjMatrix[i][j] != 0 && this.adjMatrix[i][j] == 0)
+          return false;
+      }
+    }
+    return true;
+  }
+
+  public ArrayList<Integer> busca_largura(int s){
+
+    int[] desc = new int[this.countNodes];
+    
+    ArrayList<Integer> q = new ArrayList<>();
+    ArrayList<Integer> r = new ArrayList<>();
+    q.add(s);
+    r.add(s);
+    desc[s] = 1;
+
+    while(q.size() > 0){
+      int u = q.remove(0);
+      for (int v=0; v<this.adjMatrix[u].length; v++ ) {
+        if(this.adjMatrix[u][v] != 0){
+          if(desc[v] == 0){
+            q.add(v);
+            r.add(v);
+            desc[v]=1;
+          }
+          
+        }
+      }
+    }
+    return r;
+  }
+
+  public boolean connected(){
+    return this.busca_largura(0).size() == this.countNodes;
+  }
+
 
   public int getcountNodes(){
     return this.countNodes;
@@ -59,3 +151,9 @@ class graph{
   }
   
 }
+
+/*
+Grafo de estados (velha, xadrez)
+Nó = Estado
+
+*/
